3d06300f946e7977e23db03062885cf8b508ad3f
EC2 Default User <ec2-user@ip-172-31-51-115.ec2.internal>
2022-04-03 02:33:41 +0000

    genre_view

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,34 +2,34 @@ class Admin::ItemsController < ApplicationController
   def index
     @items = Item.all
   end
-  
+
   def new
     @item = Item.new
   end
 
   def create
     @item = Item.new(item_params)
-    
+
     if @item.save
       redirect_to admin_items_path
     else
-      render new
+      render :new
     end
   end
-  
+
   def show
   end
 
   def edit
   end
-  
+
   def update
   end
-  
+
   private
-  
+
   def item_params
-    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active)
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active, :image)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: "homes#top"
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
-    
+    resources :genres,only: [:index, :create, :edit, :update]
   end
-  
+
   devise_for :admin, skip:[:registrations,:passwords],controllers:{
     sessions: "admin/sessions"
   }
